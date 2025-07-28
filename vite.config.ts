0bdb053377e3de23b0cc0a00f79b4e89bc4d4ded
Metaer <2839905270@qq.com>
2025-07-28 21:14:19 +0800

    remove google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import { VitePluginRadar } from 'vite-plugin-radar'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
@@ -25,9 +24,6 @@ export default defineConfig({
         // Since `fs` is not supported in browsers, we can use the `memfs` package to polyfill it.
         // fs: 'memfs',
       },
-    }),
-    VitePluginRadar({
-      analytics: { id: `G-7NZL3PZ0NK` },
     }),
     process.env.ANALYZE === `true`
     && visualizer({ emitFile: true, filename: `stats.html` }),
