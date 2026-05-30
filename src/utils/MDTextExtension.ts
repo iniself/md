00c50f4644f49ea72c4105ede1b55386ea8ab449
Metaer <2839905270@qq.com>
2026-05-30 23:36:01 +0800

    fix(text-extension): trim content before rendering to avoid unintended whitespace

--- src/utils/MDTextExtension.ts
+++ src/utils/MDTextExtension.ts
@@ -61,7 +61,7 @@ export default function markedTextExtension(options?: { mode?: `default` | `info
               bgColor,
               fontSize,
               text: content,
-              tokens: this.lexer.inlineTokens(content) || [],
+              tokens: this.lexer.inlineTokens(content.trim()) || [],
             }
           }
         },
